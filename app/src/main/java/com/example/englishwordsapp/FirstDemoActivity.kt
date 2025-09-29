7f2a19cbea4e440f9a4a1a9e7be79f4b68b96aa3
XataB <excist97@mail.ru>
2025-09-29 17:17:26 +0300

    Жизненный цикл

--- app/src/main/java/com/example/englishwordsapp/FirstDemoActivity.kt
+++ app/src/main/java/com/example/englishwordsapp/FirstDemoActivity.kt
@@ -2,8 +2,12 @@ package com.example.englishwordsapp
 
 import android.content.Intent
 import android.os.Bundle
+import android.os.Parcel
+import android.os.Parcelable
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.os.bundleOf
 import com.example.englishwordsapp.databinding.ActivityFirstDemoBinding
+import java.io.Serializable
 
 class FirstDemoActivity : AppCompatActivity() {
 
@@ -22,12 +26,75 @@ class FirstDemoActivity : AppCompatActivity() {
         // 2. с передачей данных
         // 3. с ожиданием результата
 
+        val word = ExtraWord(
+            "Kamilla",
+            "Камилла"
+        )
+
         binding.btnOpenSecondActivity.setOnClickListener {
-            val intent = Intent(this@FirstDemoActivity, SecondDemoActivity::class.java)
-            intent.putExtra("EXTRA_KEY_TEXT", "don't panic")
-            intent.putExtra("EXTRA_KEY_NUMBER", 42)
+            val intent = Intent(this@FirstDemoActivity, SecondDemoActivity::class.java).apply {
+                putExtra("EXTRA_KEY_TEXT", "don't panic")
+                putExtra("EXTRA_KEY_NUMBER", 42)
+                putExtra("EXTRA_KEY_WORD", word)
+            }
+
+//            val bundle = Bundle()
+//            bundle.putString("EXTRA_KEY_TEXT", "don't panic")
+//            bundle.putInt("EXTRA_KEY_NUMBER", 41)
+//            bundle.putSerializable("EXTRA_KEY_WORD", word) // Parcelable
+//            intent.putExtras(bundle)
+            intent.putExtras(
+                bundleOf(
+                    "EXTRA_KEY_TEXT" to "don't panic",
+                    "EXTRA_KEY_NUMBER" to 41,
+                    "EXTRA_KEY_WORD" to word,
+
+                    )
+            )
+
             startActivity(intent)
         }
     }
+    // Serializable
+    // Parcelable
+    // Bundle
+
+//    data class ExtraWord(
+//        val original: String,
+//        val translate: String,
+//        var learned: Boolean = false,
+//    ) : Parcelable {
+//        override fun describeContents(): Int {
+//            return 0
+//        }
+//
+//        override fun writeToParcel(dest: Parcel, flags: Int) {
+//            dest.writeString(original)
+//            dest.writeString(translate)
+//            dest.writeByte(if (learned) 1 else 0)
+//        }
+//
+//        constructor(parcel: Parcel) : this(
+//            original = parcel.readString().toString(),
+//            translate = parcel.readString().toString(),
+//            learned = parcel.readByte() != 0.toByte()
+//        )
+//
+//        companion object CREATOR : Parcelable.Creator<ExtraWord> {
+//            override fun createFromParcel(source: Parcel): ExtraWord? {
+//                return ExtraWord(source)
+//            }
+//
+//            override fun newArray(size: Int): Array<out ExtraWord?>? {
+//                return arrayOfNulls(size)
+//            }
+//        }
+//    }
+
+    data class ExtraWord(
+        val original: String,
+        val translate: String,
+        var learned: Boolean = false,
+    ) : Serializable
 
 }
--- app/src/main/java/com/example/englishwordsapp/SecondDemoActivity.kt
+++ app/src/main/java/com/example/englishwordsapp/SecondDemoActivity.kt
@@ -1,16 +1,31 @@
 package com.example.englishwordsapp
 
+import android.Manifest.permission.ACCESS_FINE_LOCATION
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.englishwordsapp.FirstDemoActivity.ExtraWord
 import com.example.englishwordsapp.databinding.ActivitySecondDemoBinding
+
 
 class SecondDemoActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivitySecondDemoBinding
+
+    private val locationPermissionLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()) {
+            isGranted ->
+        if (isGranted) {
+            Log.d("SecondDemoActivity", "Разрешение на локацию получено")
+        } else {
+            Log.d("SecondDemoActivity", "Разрешение на локацию отклонено")
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,18 +33,71 @@ class SecondDemoActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(binding.root)
 
+        Log.i("!!!", "${this.componentName.shortClassName} Выполняется метод onCreate() ")
+
+        val btnLocation = binding.btnRequestPermission
+        btnLocation.setOnClickListener {
+            locationPermissionLauncher.launch(ACCESS_FINE_LOCATION)
+        }
+
         with(binding) {
             btnOpenFirstActivity.setOnClickListener {
                 val intent = Intent(this@SecondDemoActivity, FirstDemoActivity::class.java)
                 startActivity(intent)
             }
 
-            val text = intent.getStringExtra("EXTRA_KEY_TEXT")
-            val number = intent.getIntExtra("EXTRA_KEY_NUMBER", 0)
+//            val text = intent.getStringExtra("EXTRA_KEY_TEXT")
+//            val number = intent.getIntExtra("EXTRA_KEY_NUMBER", 0)
+//            val word: ExtraWord = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+//                intent.getSerializableExtra("EXTRA_KEY_WORD", ExtraWord::class.java) as ExtraWord
+//            } else {
+//                intent.getSerializableExtra("EXTRA_KEY_WORD") as ExtraWord// deprecated
+//            }
 
+//            val word = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+//                intent.getParcelableExtra("EXTRA_KEY_WORD", ExtraWord::class.java)
+//            } else {
+//                intent.getParcelableExtra("EXTRA_KEY_WORD")
+//            }
+
+            val bundle = intent.extras
+            val text = bundle?.getString("EXTRA_KEY_TEXT")
+            val number = bundle?.getInt("EXTRA_KEY_NUMBER")
+            val word = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                bundle?.getSerializable("EXTRA_KEY_WORD", ExtraWord::class.java)
+            } else {
+                bundle?.getSerializable("EXTRA_KEY_WORD") as ExtraWord
+            }
+
+            tvExtraWord.text = word?.original
             tvString.text = text
             tvNumber.text = number.toString()
 
         }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.i("!!!", "${this.componentName.shortClassName} Выполняется метод onStart() ")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.i("!!!", "${this.componentName.shortClassName} Выполняется метод onResume() ")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.i("!!!", "${this.componentName.shortClassName} Выполняется метод onPause() ")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.i("!!!", "${this.componentName.shortClassName} Выполняется метод onStop() ")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i("!!!", "${this.componentName.shortClassName} Выполняется метод onDestroy() ")
     }
 }
